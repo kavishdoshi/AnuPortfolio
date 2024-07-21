3f75ba7eea5720c048762b998d704ef901995336
sherlockof96 <kavishdoshi28@gmail.com>
2024-07-21 11:53:52 -0700

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
         <Layout classname='py-8 flex items-center justify-between lg:flex-col lg:py-6'>
             <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
             <div className='flex items-center lg:py-2'>
-                Build With <span className='text-primary dark:text-primaryDark text-2xl px-1'>&#9825;</span>by&nbsp; <Link href="www.kavishdoshi.com" className='underline underline-offset-2'>Kavish</Link>
+                Build With <span className='text-primary dark:text-primaryDark text-2xl px-1'>&#9825;</span>by&nbsp; <a href="https://www.kavishdoshi.com" className='underline underline-offset-2'>Kavish</a>
             </div>
             <Link href="/" className='underline underline-offset-2'>Say Hello</Link>
         </Layout>
